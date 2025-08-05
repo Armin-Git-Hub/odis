be027e3204c7fa4b7cc0c19834d4327c3fae1ec1
Armin <arnimstrupp@gmail.com>
2025-08-05 18:41:15 +0200

    New create graph from concepts
    Creates a new graph struct from concepts and their context.
    Includes the upper neighbor function to determine edge connection.

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -2,47 +2,44 @@ use bit_set::BitSet;
 
 use crate::FormalContext;
 
-pub mod next_closure;
-pub mod fcbo;
-pub mod canonical_basis;
 pub mod attribute_exploration;
+pub mod canonical_basis;
+pub mod fcbo;
+pub mod next_closure;
+pub mod upper_neighbor;
 
 impl<T> FormalContext<T> {
-    pub fn index_concepts<'a>(
-        &'a self,
-    ) -> impl Iterator<Item = (BitSet, BitSet)> + 'a {
+    pub fn index_concepts<'a>(&'a self) -> impl Iterator<Item = (BitSet, BitSet)> + 'a {
         next_closure::concepts(&self)
     }
 }
 
 impl<T> FormalContext<T> {
-    pub fn fcbo_index_concepts<'a>(
-        &'a self,
-    ) -> impl Iterator<Item = (BitSet, BitSet)> + 'a {
+    pub fn fcbo_index_concepts<'a>(&'a self) -> impl Iterator<Item = (BitSet, BitSet)> + 'a {
         fcbo::fcbo_concepts(&self)
     }
 }
 
 impl<T> FormalContext<T> {
-    pub fn canonical_basis<'a>(
-        &'a self,
-    ) -> Vec<(BitSet, BitSet)> {
+    pub fn canonical_basis<'a>(&'a self) -> Vec<(BitSet, BitSet)> {
         canonical_basis::canonical_basis(&self)
     }
 }
 
 impl<T> FormalContext<T> {
-    pub fn canonical_basis_optimised<'a>(
-        &'a self,
-    ) -> Vec<(BitSet, BitSet)> {
+    pub fn canonical_basis_optimised<'a>(&'a self) -> Vec<(BitSet, BitSet)> {
         canonical_basis::canonical_basis_optimised(&self)
     }
 }
 
 impl FormalContext<String> {
-    pub fn attribute_exploration<'a>(
-        &mut self,
-    ) -> Vec<(BitSet, BitSet)> {
+    pub fn attribute_exploration<'a>(&mut self) -> Vec<(BitSet, BitSet)> {
         attribute_exploration::attribute_exploration(self)
+    }
+}
+
+impl<T> FormalContext<T> {
+    pub fn upper_neighbor<'a>(&self, input: &BitSet) -> BitSet {
+        upper_neighbor::upper_neighbor(input, &self)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod algorithms;
 mod data_structures;
 
 pub use data_structures::formal_context::FormalContext;
+pub use data_structures::graph::Graph;
